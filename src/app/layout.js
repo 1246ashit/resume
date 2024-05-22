0d5b4940af6f0c2301c307a84e0818f2ccbaddb0
jj <1246ashit@gmail.com>
2024-05-22 14:35:06 +0800

    換 title icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import OpenNB from "./components/OpenNB";
 const Scene = dynamic(() => import("./components/Scene"), { ssr: false });
 import Contents from "./components/Contents";
 import { Toaster } from "react-hot-toast";
+import Image from "next/image";
 
 export default function RootLayout({ children }) {
   const [showCard, setShowCard] = useState(false);
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>俊杰的履歷</title>
+        <link rel="icon" href="./myicon.svg" type="image/x-icon"/>
       </head>
       <body className="relative h-screen w-screen bg-slate-950">
         <Toaster position="top-center" />
